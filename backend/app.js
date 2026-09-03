7bbc150578f9d166045aad6fcc7f370e7a77f400
ManhVN <manhvanngo16052006@gmail.com>
2026-09-03 12:31:33 +0700

    Ready for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,28 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoute from "./routes/authRoute.js";
 
 const app = express();
 
+const ___dirname = path.resolve();
 app.use("/api/auth", authRoute);
 
 dotenv.config();
-// eslint-disable-next-line no-undef
+
 const PORT = process.env.PORT || 5000;
-// eslint-disable-next-line no-unused-vars
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(___dirname, "../frontend/dist")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(___dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 const server = app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
 });
