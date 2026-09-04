d1310c270967a3e967ef3709dbc2db71d8b3507b
ManhVN <manhvanngo16052006@gmail.com>
2026-09-04 22:11:27 +0700

    Add protection rate limit and detect bot spam

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -6,13 +6,16 @@ import {
   updateProfile,
 } from "../controller/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
+import arcjetMiddleware from "../middleware/arcjetMiddleware.js";
 
 const authRoute = Router();
+
+authRoute.use(arcjetMiddleware);
 
 authRoute.post("/sign-in", signin);
 authRoute.post("/sign-out", signout);
 authRoute.post("/sign-up", signup);
 
-authRoute.post("/update-profile", authMiddleware, updateProfile);
+authRoute.put("/update-profile", authMiddleware, updateProfile);
 
 export default authRoute;
